77e0818201e2e7427dfd6a9b111b04dc4cce7cab
Nadim Hoque <nadimhoque@gmail.com>
2017-03-11 14:34:47 -0700

    I am now changing direction of the code. Instead of running grep based off of the first Popen command I am going to have python itself process the output and filter based off of "kernel-[0-9]". I have written a function within that will actually grab the kernel versions and possibly find the oldest that way.

--- oldest_removed/oldest_removed.py
+++ oldest_removed/oldest_removed.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 
 import subprocess
+import re
 
+kernellist = []
 #subprocess.call(["rpm","-qa"])
+
+def oldestkernel(kernellist):
+	print "hello"
+
 
 rpmout = subprocess.Popen(('rpm','-qa'),stdout=subprocess.PIPE)
 
-subprocess.Popen(('grep','kernel-[0-9]'),stdin=rpmout.stdout)
+#kernels = subprocess.Popen(('grep','kernel-[0-9]'),stdin=rpmout.stdout.read())
+
+for items in iter(rpmout.stdout.readline, ''):
+	print items
