25f00220b0e77216850a2e8b2805ea5fa3b5d9c9
Nadim Hoque <nadimhoque@gmail.com>
2017-03-12 14:11:44 -0700

    I am using the regex branch for now. So I used the re.findall method and actually filtered out the kernel package itself. I need to figure out why it is returning all the unmatched strings because if you run the script then the number of elements in the array are the same as the number returned when executed the rpm -qa command. For now it does not matter since it is printing the kernel packag so for now I will work on the oldestkernel function and have it find the oldest one based off of version number.

--- oldest_removed/oldest_removed.py
+++ oldest_removed/oldest_removed.py
@@ -19,7 +19,6 @@ rpmout = subprocess.Popen(('rpm','-qa'),stdout=subprocess.PIPE)
 #kernels = subprocess.Popen(('grep','kernel-[0-9]'),stdin=rpmout.stdout.read())
 
 for items in iter(rpmout.stdout.readline, ''):
-	if "kernel" in items:
-		kernellist.append(items.split("\n"))
+	kernellist.append(re.findall('^kernel-[0-9].+',items))
 
 oldestkernel(kernellist)
