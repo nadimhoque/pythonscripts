7492cc706c660679070e4b6c9ef29c2eab624e25
Nadim Hoque <nadimhoque@gmail.com>
2017-03-13 23:30:37 -0700

    I finally fixed the issue the the empty strings being appeneded now i can process the elements in each string

--- oldest_removed/oldest_removed.py
+++ oldest_removed/oldest_removed.py
@@ -5,20 +5,21 @@ import re
 import string
 
 kernellist = []
+
 #subprocess.call(["rpm","-qa"])
 
 def oldestkernel(kernellist):
-	#kernelout = re.search('(^kernel)',kernellist)
-	#kernelout.group(0)
-	print kernellist
-	
+		print kernellist
+
 
 
 rpmout = subprocess.Popen(('rpm','-qa'),stdout=subprocess.PIPE)
 
-#kernels = subprocess.Popen(('grep','kernel-[0-9]'),stdin=rpmout.stdout.read())
 
 for items in iter(rpmout.stdout.readline, ''):
-	kernellist.append(re.findall('^kernel-[0-9].+',items))
+	#kernellist.append(re.findall('^kernel-[0-9].+',items))
+	temp = re.findall('^kernel-[0-9].+',items)
+	if temp:
+		kernellist.append(temp[0])
 
 oldestkernel(kernellist)
