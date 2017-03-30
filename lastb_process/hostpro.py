8ea4395879c6beaeddeb038e6f231a7447abdced
Nadim Hoque <nadimhoque@gmail.com>
2017-03-30 00:11:59 -0700

    I finished the hostonly function in the hostpro file so now only the hostname is printed. I also created numberof function which counts the occurance of a hostname/ip using the Counter object from the collections class. In the lastb_process.py I added the case if the selection withing the second option to allow the user to select the hostonly and the occurance. I have yet to complete the 4 main menu item. The issue is that the main program is not progressing to that menue selection which i think might be due to formatting issues

--- lastb_process/hostpro.py
+++ lastb_process/hostpro.py
@@ -1,26 +1,62 @@
 #!/usr/bin/python
 import re
-def hostlist(mainlist):
-	host = []
-	for items in mainlist:
-		if items not in host:
-			host.append(items)
-	for output in host:
-		print output
+from collections import Counter
 
+
+# this function prints out all unique hostnames and ip addresses
+def hostlist(mainlist):
+    host = []
+    for items in mainlist:
+        if items not in host:
+            host.append(items)
+    for output in host:
+        print output
+
+
+# this function prints all unique ip adddresses
 def iponly(mainlist):
-	ips =[]
-	pattern = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
-	for items in mainlist:
-		if items not in ips:
-			if pattern.match(items):
-				ips.append(items)
-	for output in ips:
-		print output
-			
+    ips = []
+    pattern = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+    for items in mainlist:
+        if items not in ips:
+            if pattern.match(items):
+                ips.append(items)
+    for output in ips:
+        print output
+
+
+def hostonly(mainlist):
+    hosts = []
+    pattern = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+    for items in mainlist:
+        if items not in hosts:
+            if not pattern.match(items):
+                hosts.append(items)
+    for output in hosts:
+        print output
+
+
+# this function prints all the users who attempted to log in
 def userlist(mainlist):
-	users = []
-	for items in mainlist:
-		if items not in users:
-			users.append(items)
-	print users
+    users = []
+    for items in mainlist:
+        if items not in users:
+            users.append(items)
+    for items in users:
+		print items
+
+
+def numberof(mainlist):
+    # print "running numberof function"
+    # print mainlist
+    listm = Counter(mainlist)
+    for key, value in listm.items():
+        print(key, value)
+    # for items in listm:
+    # print items
+    # counted = dict()
+    # for items in mainlist:
+    # if items not in counted:
+    # counted[items] = 1
+    # if items in counted:
+    # counted[items] += 1
--- lastb_process/lastb_process.py
+++ lastb_process/lastb_process.py
@@ -1,54 +1,69 @@
 #!/usr/bin/python
 import hostpro
 
+
 def hostlistprint():
-	print "Please select One of the following options:"
-	print "1: All hostnames and IP addresses"
-	print "2: Only IP addresses"
-	print "3: Only hostnames"
+    print "Please select One of the following options:"
+    print "1: All hostnames and IP addresses"
+    print "2: Only IP addresses"
+    print "3: Only hostnames"
+    print "4: Number of occurances"
+
 
 def printmenu():
-	print "Please select the following"
-	print "1: Get all the users who attempted to login"
-	print "2: Hostname/IP information (This will display another menu)"
-	print "3: Get the start time of the login attempt"
-	print "4: Print the whole log as is"
-	print "q: Exits the program"
-	
+    print "Please select the following"
+    print "1: Get all the users who attempted to login"
+    print "2: Hostname/IP information (This will display another menu)"
+    print "3: Get the start time of the login attempt"
+    print "4: Print the whole log as is"
+    print "q: Exits the program"
+
+
 def main():
-	mainlist = []
-	hostnames = []
-	user = []
-	time = []
-	selection = "g"
-	hostsel = "g"
+    mainlist = []
+    hostnames = []
+    user = []
+    time = []
+    selection = "g"
+    hostsel = "g"
 
-# the below lines are simply parsing the colums
-	log = open('lastb.log','r')
+    # the below lines are simply parsing the colums
+    log = open('lastb.log', 'r')
 
-	for lines in log:
-		mainlist.append(lines)
-		hostnames.append(lines.split()[2])
-		user.append(lines.split()[0])
-		time.append(lines.split()[6])
-		#date.append(lines.split()[4])
+    for lines in log:
+        mainlist.append(lines)
+        hostnames.append(lines.split()[2])
+        user.append(lines.split()[0])
+        time.append(lines.split()[6])
+    # date.append(lines.split()[4])
 
-	while selection != "q":
-		printmenu()
-		selection = raw_input("Please enter your selection now:")
+    while selection != "q":
+        printmenu()
+        selection = raw_input("Please enter your selection now:")
 
-		if selection == "1":
-			hostpro.userlist(user)
+        if selection == "1":
+            hostpro.userlist(user)
 
-		if selection == "2":
-			hostlistprint()
-			hostsel = raw_input("Please enter one of the following:")
-			if hostsel == "1":
-				hostpro.hostlist(hostnames)
-			if hostsel == "2":
-				hostpro.iponly(hostnames)
+        if selection == "2":
+            hostlistprint()
+            hostsel = raw_input("Please enter one of the following:")
+            if hostsel == "1":
+                hostpro.hostlist(hostnames)
+            if hostsel == "2":
+                hostpro.iponly(hostnames)
+            if hostsel == "3":
+                hostpro.hostonly(hostnames)
+            if hostsel == "4":
+                print "Running Numberof function"
+                hostpro.numberof(hostnames)
 
-	log.close()
+		if selection == "4":
+			log.seek(0)
+			for lines in log:
+				print lines
+
+    log.close()
+
 
 if __name__ == "__main__":
-	main()
+    main()
